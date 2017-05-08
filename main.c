4a639825cf16bbdb2e234d3f75ae40b2c47aeb47
Simon <simon.wiederkehr@ruag.com>
2017-05-08 15:16:20 +0200

    dies ist eine neue version
    Hallo hier ist eine neue veersion

--- main.c
+++ main.c
@@ -33,6 +33,26 @@ int main(void)
 		{
 			TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
 		}
+				int8_t EEWriteByte(uint16_t u16addr, uint8_t u8data)
+		{
+			TWIStart();
+			if (TWIGetStatus() != 0x08)
+				return ERROR;
+			//select devise and send A2 A1 A0 address bits
+			TWIWrite((EEDEVADR)|(uint8_t)((u16addr & 0x0700)>>7));
+			if (TWIGetStatus() != 0x18)
+				return ERROR;  
+			//send the rest of address
+			TWIWrite((uint8_t)(u16addr));
+			if (TWIGetStatus() != 0x28)
+				return ERROR;
+			//write byte to eeprom
+			TWIWrite(u8data);
+			if (TWIGetStatus() != 0x28)
+				return ERROR;
+			TWIStop();
+			return SUCCESS;
+		}
 
 
 	} // end of while
